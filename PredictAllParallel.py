fe14e61a9055bd808e3bb184984fd2ba966044a3
Andrea Battistello <Andrea Battistello>
2017-06-07 23:01:27 +0200

    Added script to predict in parallel CPU and GPU

---
+++ PredictAllParallel.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+import numpy as np
+from time import time
+from threading import Thread
+from keras.models import load_model
+import glob
+import pickle
+import os
+import math
+from keras.datasets import cifar10
+from sklearn.metrics import accuracy_score
+
+BATCH_SIZE = 32
+CPU_BATCHES = 20
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+x_test = x_test.astype('float32')
+x_test /= 255
+
+
+
+def split_in_batches(data, batch_size):
+    ret = []
+    start = 0
+    end = 0
+    num_batches = math.ceil(data.shape[0] / batch_size)
+    for batch_num in range(num_batches):
+        end += batch_size
+        if end > data.shape[0]:  #last batch
+            ret.append(data[-batch_size:])
+        else:
+            ret.append(data[start:end])
+        start = end
+    return np.array(ret)
+
+
+def run_prediction(session, y, data, x):
+	for batch in data:
+		session.run(y, feed_dict={x : batch})
+
+
+def load_model_cpu_gpu(modelname):
+    with tf.device('/cpu:0'):
+        x = tf.placeholder(name='x', dtype=tf.float32,  shape=(None, 32,32,3))
+
+    with tf.device('/gpu:0'):
+        model_gpu = load_model(modelname)
+        gpu = model_gpu(x)
+        
+    with tf.device('/cpu:0'):
+        model_cpu = load_model(modelname)
+        cpu = model_cpu(x)
+        
+    return cpu, gpu, x
+   
+def predict_cpu_gpu(cpu, data_cpu, gpu, data_gpu, x):
+    with tf.Session(config=tf.ConfigProto(log_device_placement=True, intra_op_parallelism_threads=8)) as sess:
+        sess.run(tf.global_variables_initializer())
+
+        coord = tf.train.Coordinator()
+
+        threads = []
+
+        # comment out 0 or 1 of the following 2 lines:
+        threads += [Thread(target=run_prediction, args=(sess, cpu, data_cpu, x))]
+        threads += [Thread(target=run_prediction, args=(sess, gpu, data_gpu, x))]
+
+        t0 = time()
+
+        for t in threads:
+            t.start()
+
+        coord.join(threads)
+
+        t1 = time()
+
+
+    return (t1 - t0)
+
+def predict_parallel(modelname, batch_size, data, cpu_percentage = [0.5], nruns = 1):
+    batches = split_in_batches(data, batch_size)
+    nbatches = len(batches)
+    cpu, gpu, x = load_model_cpu_gpu(modelname)
+    
+    for cpu_perc in cpu_percentage:
+        batches_cpu = int(nbatches * cpu_perc)
+        data_cpu = batches[:batches_cpu]
+        data_gpu = batches[batches_cpu:]
+        times = []
+        for run in range(nruns):
+            print ("Running run {0} using {1}% CPU".format(run, cpu_perc))
+            
+            time_spent = predict_cpu_gpu(cpu, data_cpu, gpu, data_gpu, x)
+            print(time_spent)
+            
+            times.append(time_spent)
+        
+        print ("Average time: {0}".format(sum(times)/len(times)))
+            
+predict_parallel('model1.h5', 64, x_test, nruns = 10)
+
+import gc; gc.collect()
